16cb6983db61261ca4a040419b203fe1221562ac
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-18 21:45:53 -0400

    begin implementing search

---
+++ frontend/components/search/search.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: "",
+    };
+  }
+
+
+  handleUpdate(e) {
+    this.setState({ query: e.currentTarget.value});
+  }
+
+  render() {
+    return (
+      <div className="search-container">
+        <div className="search-top">
+          <span>Search for a song or playlist.</span>
+        </div>
+        <div className="search-input">
+          <form>
+            <input
+              type="text"
+              className="search-input-form"
+              value={this.state.query}
+              onUpdate={this.handleUpdate}
+              autoFocus
+              placeholder="Search for a song..." />
+          </form>
+        </div>
+        <div className="search-results">
+          <ul>
+            Search Results
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
---
+++ frontend/components/search/search_container.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Search from './search';
+
+
+const mapStateToProps = () => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = () => {
+  return {
+
+  };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Search);
