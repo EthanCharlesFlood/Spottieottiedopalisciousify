14765da80a1b79654c88f42bca1b84f2b0d9f58d
Ethan Flood <ethan.flood@gmail.com>
2018-09-13 17:06:09 -0400

    continue working on search feature

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,7 +1,16 @@
 class Api::SearchController < ApplicationController
 
   def index
-    if params[:query]
+    render json: ["Please enter your search query."], status: 422 if params[:query].empty?
+
+    @playlists = Playlists.where()
+    @songs = Songs.where()
   end
 
+
+  private
+
+  def query_string
+
+  end
 end
