32fca4f21c3ea43a64fac962037876f07df71228
Ethan Flood <ethan.flood@gmail.com>
2018-09-18 15:19:16 -0400

    finish up search actions

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -1,3 +1,5 @@
+import * as SearchAPIUtil from "./../util/search_api_util";
+
 export const LOADING = "LOADING";
 export const LOADED = "LOADED";
 export const RECEIVE_SEARCH_RESULTS = "RECEIVE_SEARCH_RESULTS";
@@ -15,4 +17,11 @@ const clearSearchResults = () => {
   return {
     type: CLEAR_SEARCH_RESULTS
   };
+};
+
+
+export const fetchSearchResults = (query) => (dispatch) => {
+  return SearchAPIUtil.fetchSearchResults(query).then(results => {
+    return (dispatch) => receiveSearchResults(results);
+  });
 };
