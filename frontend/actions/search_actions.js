54ce18f8f77fd7a67d2f95a219cf3e85f801a52b
Ethan Flood <ethan.flood@gmail.com>
2018-09-18 15:16:21 -0400

    add search actions

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -1,2 +1,18 @@
 export const LOADING = "LOADING";
 export const LOADED = "LOADED";
+export const RECEIVE_SEARCH_RESULTS = "RECEIVE_SEARCH_RESULTS";
+export const CLEAR_SEARCH_RESULTS = "CLEAR_SEARCH_RESULTS";
+
+
+const receiveSearchResults = (results) => {
+  return {
+    type: RECEIVE_SEARCH_RESULTS,
+    results
+  };
+};
+
+const clearSearchResults = () => {
+  return {
+    type: CLEAR_SEARCH_RESULTS
+  };
+};
