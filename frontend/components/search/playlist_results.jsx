48682884126ad9b828d044ffc4d84686e4507672
Ethan Flood <ethan.flood@gmail.com>
2018-11-18 22:52:06 -0500

    add playlist rendering logic in search results

--- frontend/components/search/playlist_results.jsx
+++ frontend/components/search/playlist_results.jsx
@@ -6,9 +6,21 @@ class PlaylistResults extends React.component {
 
 
   render() {
+    const playlists = this.props.playlists.map(playlist => {
+      return (
+      <div key={playlist.id} className="playlist-result-item">
+      <div className="playlist-result-item-container" key={playlist.id}>
+        <Link key={playlist.id} to={`/playlists/${playlist.id}`}>
+          <img className="playlist-result-item-image" key={playlist.id + 1} src={playlist.imgurl}/>
+          <span className="playlist-result-item-link">{playlist.playlist_name}</span></Link>
+      </div>
+      </div>
+      );
+    });
     return (
       <div className="playlist-songs-results-container">
         <ul className="playlist-songs-results-list">
+          {playlists}
         </ul>
       </div>
     );
--- frontend/components/search/songs_results.jsx
+++ frontend/components/search/songs_results.jsx
@@ -12,14 +12,14 @@ class SongsResults extends React.component {
     const songs = this.props.song_results.map((song, idx) => {
       let songAdder = (song) => this.handleAddSong(song);
       return (
-        <li key={song.id} id={idx}  className="song-index-list-item">
-          <span key={song.id} className="song-index-list-title">
+        <li key={song.id} id={idx}  className="song-result-list-item">
+          <span key={song.id} className="song-result-list-title">
             <span key={Math.random()} onClick={songAdder} id={idx} className="play-button"><i className="fas fa-music"></i></span>
             {song.song_name}
             <br></br>
             {song.artist.artist_name} - {song.album}
           </span>
-          <span className="song-index-list-features">
+          <span className="song-result-list-features">
             <button key={song.id} id={song.id} className="song-modal" onClick={this.openModal}><i className="fas fa-ellipsis-h"></i></button>
             {"4'33'"}
           </span>
