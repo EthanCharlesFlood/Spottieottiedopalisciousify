de4140f75aed643c18a62ef222e56d00c1a9dea9
Ethan Flood <ethan.flood@gmail.com>
2018-11-28 22:14:46 -0500

    make changes to search controller

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -2,15 +2,11 @@ class Api::SearchController < ApplicationController
 
   def index
     render json: ["Please enter your search query."], status: 422 if params[:query].empty?
+    @query = params[:query]
 
-    @playlists = Playlists.where()
-    @songs = Songs.where()
+
+    @playlists = Playlists.where('lower(playlist_name) LIKE ?', "#{@query}")
+    @songs = Songs.where('lower(song_name) LIKE ?', "#{@query}")
   end
 
-
-  private
-
-  def query_string
-    
-  end
 end
