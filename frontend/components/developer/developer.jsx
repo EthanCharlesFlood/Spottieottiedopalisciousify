9baabbb0223a2bf81b1f8568befae2c48210524f
Ethan Flood <ethan.flood@gmail.com>
2019-01-05 18:46:10 -0500

    work on developer page

--- frontend/components/developer/developer.jsx
+++ frontend/components/developer/developer.jsx
@@ -4,4 +4,17 @@ import { Link } from 'react-router-dom';
 
 class Developer extends React.Component {
 
+
+
+  render() {
+    return (
+      <div className="developer-container">
+        <img className="developer-picture" src="app/assets/images/Ethan.jpeg"></img>
+        <span className="developer-name">Developer Name</span>
+        <a href="https://github.com/EthanCharlesFlood" className="developer-a">Github</a>
+        <a href="https://www.linkedin.com/in/ethan-flood-58239589/" className="developer-a">Linkedin</a>
+        <a href="" className="developer-a">Resume</a>
+      </div>
+    );
+  }
 }
