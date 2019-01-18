8a61eddd38b81a42da6634d863f177dccb879293
Ethan Flood <ethan.flood@gmail.com>
2019-01-17 20:18:00 -0500

    add stuff

---
+++ frontend/components/songs/songs_index_item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class SongIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      icon: '<i className="fas fa-play"></i>',
+    };
+  }
+
+
+  render() {
+
+    return ();
+  }
+}
