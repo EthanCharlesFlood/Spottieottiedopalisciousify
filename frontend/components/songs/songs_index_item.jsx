a3569eb3ddea88190bc533134ab757605c80acae
Ethan Flood <ethan.flood@gmail.com>
2019-01-17 20:21:11 -0500

    add song adder to index item component

--- frontend/components/songs/songs_index_item.jsx
+++ frontend/components/songs/songs_index_item.jsx
@@ -5,11 +5,16 @@ class SongIndexItem extends React.Component {
   constructor(props) {
     super(props);
 
+
+    this.handleAddSong = this.handleAddSong.bind(this);
     this.state = {
       icon: '<i className="fas fa-play"></i>',
     };
   }
 
+  handleAddSong(e) {
+    this.props.queueSong(this.props.songs[e.currentTarget.id]);
+  }
 
   render() {
 
