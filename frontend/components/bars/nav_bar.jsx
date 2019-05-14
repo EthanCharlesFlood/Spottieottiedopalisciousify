17b45c8e38bf8727aef6e7cb9335c9b803a27214
Ethan Flood <ethan.flood@gmail.com>
2019-05-14 16:55:55 -0400

    work on navbar conditional underlines

--- frontend/components/bars/nav_bar.jsx
+++ frontend/components/bars/nav_bar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 
 const NavBar = () => {
-  return (
-    <div className="navbar-container">
-      <Link className="navbar-button" to="/">Playlists</Link>
-      <Link className="navbar-button" to="/songs">Songs</Link>
-    </div>
-  );
+    if (this.props.history) {
+      return (
+        <div className="navbar-container">
+          <Link className="navbar-button" to="/">Playlists</Link>
+          <Link className="navbar-button-underlined" to="/songs">Songs</Link>
+        </div>
+      );
+    } else if (this.props.history){
+        return (
+          <div className="navbar-container">
+            <Link className="navbar-button-underlined" to="/">Playlists</Link>
+            <Link className="navbar-button" to="/songs">Songs</Link>
+          </div>
+        );
+      }
 };
 
 
-export default NavBar;
+export default withRouter(NavBar);
