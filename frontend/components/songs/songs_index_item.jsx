0a88495bb18c397ccd3e5ef61b56d65b9ebadc79
Ethan Flood <ethan.flood@gmail.com>
2019-05-14 20:43:52 -0400

    work on song index item

--- frontend/components/songs/songs_index_item.jsx
+++ frontend/components/songs/songs_index_item.jsx
@@ -12,12 +12,36 @@ class SongIndexItem extends React.Component {
     };
   }
 
+  onMouseEnter() {
+    this.setState({ icon: '<i className="fas fa-play"></i>' });
+  }
+
+  onMouseLeave() {
+    this.setState({ icon: '<i className="fas fa-music"></i>' });
+  }
+
   handleAddSong(e) {
     this.props.queueSong(this.props.songs[e.currentTarget.id]);
   }
 
   render() {
-
-    return ();
+    const song = this.props.song;
+    return (
+      <li key={song.id} id={this.props.idx}  className="song-index-list-item">
+        <span key={song.id} className="song-index-list-title">
+          <span key={Math.random()}
+            onClick={this.handleAddSong}
+            id={this.props.idx}
+            className="play-button"><i className="fas fa-play"></i></span>
+          {song.song_name}
+          <br></br>
+          {song.artist.artist_name} - {song.album}
+        </span>
+        <span className="song-index-list-features">
+          <button key={song.id} id={song.id} className="song-modal" onClick={this.openModal}><i className="fas fa-plus"></i></button>
+          {this.durationParser(song.duration)}
+        </span>
+      </li>
+    );
   }
 }
